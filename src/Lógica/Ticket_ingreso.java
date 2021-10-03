9aaef27f9c55e50a09e17e9d98ce4a8508a1d373
ANDRES <ANDRES@DESKTOP-NRKDC2R>
2021-10-03 01:51:47 -0500

    Ticket ingreso repared

---
+++ src/Lógica/Ticket_ingreso.java
@@ -0,0 +1,18 @@
+package Lógica;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Ticket_ingreso {
+    
+    private Date  Fecha_Ingreso;
+    private String idticked,idpuesto,placa;
+
+public Ticket_ingreso(){
+    this.idticked = "N-"+UUID.randomUUID().toString().substring(0, 3);
+}
+    public String getIdticket(){
+         return idticked; 
+    }
+  
+}
